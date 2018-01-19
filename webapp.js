922b8c2cd71da1915f7c7a92f9748cef0e1d6120
SiddharthDinkar <siddharth.dinkar@warriorlife.net>
2018-01-19 14:49:20 -0800

    attemped making a button

--- webapp.js
+++ webapp.js
@@ -1,7 +1,12 @@
 // JavaScript Document
-var score= 1; 
+var score= 0; 
 function cookie_clicker() {
 	score= score+ 1;
 	document.getElementById("score").value=score;
-	
 }
+
+function chef() {
+	window.setInterval(score + 1, 1000);
+	document.getElementById("score").value=score;	
+}               
+
