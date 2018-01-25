50b148af8f53da7314cd6044f2fbaf39faabb278
SiddharthDinkar <siddharth.dinkar@warriorlife.net>
2018-01-25 14:39:42 -0800

    added chef function

--- webapp.js
+++ webapp.js
@@ -4,6 +4,7 @@
 var score= 0; 
 var grill = 0;
 var chef = 0;
+var trap = 0;
 
 function cookie_clicker() {
 	if( grill >= 0 ) {
@@ -11,8 +12,7 @@ function cookie_clicker() {
 	} else {
 		score++;
 	}
-	document.getElementById("score").value=score;
-
+	document.getElementById("score").value=score;                           
 }
 
 // for buying items
@@ -23,12 +23,20 @@ function shop(sel) {
 				score-=100;
 				grill++;
 			}
+			break;
 		case "chef":
+			if(score >= 150) {
+				score-=150;
+				chef++;	
+			}
+			break;
 	}
+	document.getElementById("score").value=score; 
 }
 
 // dispense bonuses every second
 setInterval(bonus, 1000);
 function bonus() {
-	score+=grill * 
+	score+=chef*1;
+	document.getElementById("score").value=score; 
 }
